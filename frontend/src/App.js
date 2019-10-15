d594d1649a98152c8e8e943ca278debf1c0e3d4b
Mansoor <mansoor.akhtar@zeptosystems.com>
2019-10-15 19:08:14 +0500

    Redux setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
 import './App.css';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import Layout from './components/hoc/Layout/Layout';
+import store from './store';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+        <BrowserRouter>
+          <Layout>
+            <Switch>
+            </Switch>
+          </Layout>
+        </BrowserRouter>
+    </Provider>
   );
 }
 
