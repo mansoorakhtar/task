256934fad840cd50a4f6b5a28244e06b79918045
Mansoor <mansoor.akhtar@zeptosystems.com>
2019-10-15 18:35:16 +0500

    Frontend package installation

--- backend/public/index.php
+++ backend/public/index.php
@@ -9,6 +9,11 @@
 
 define('LARAVEL_START', microtime(true));
 
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-CSRF-Token');
+header('Access-Control-Allow-Credentials: true');
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
