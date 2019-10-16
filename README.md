cfe58e577606cb1072499d8c7dee012559850820
Mansoor <mansoor.akhtar@zeptosystems.com>
2019-10-16 20:59:55 +0500

    Database SQL dump added, deployment instruction added

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# task
+# Code Challenge
+=====================================================
+
+## Quick Start
+
+### Server Configuration
+
+* Create database "task"
+* Import Database which is located in database folder
+* You can also import database through migration, CD to backend directory and run command
+```php artisan migrate``` this command will import database.
+* Clone Project to local,CD to backend directory and run composer update command
+* Add virtual host, the one i have used was *http://task.local/api*
+* Add entry in httpd-vhosts.conf file
+```
+<VirtualHost task.local:80>
+	DocumentRoot "path-to-htdocs\project-name\backend\public"
+	ServerName task.local
+	<Directory "path-to-htdocs\project-name\backend\public">
+		Order allow,deny
+		Allow from all
+	</Directory>
+</VirtualHost>
+
+<VirtualHost task.local:443>
+	DocumentRoot "path-to-htdocs\project-name\backend\public"
+	ServerName task.local
+	SSLEngine On
+	SSLCertificateFile "path-to-apache\conf\ssl.crt\server.crt"
+	SSLCertificateKeyFile "path-to-apache\conf\ssl.key\server.key"
+	<Directory "path-to-htdocs\project-name\backend\public">
+		Order allow,deny
+		Allow from all
+	</Directory>
+</VirtualHost>
+``` 
+*  Add entry in hosts file located in
+
+```C:\Windows\System32\drivers\etc```
+
+* If you want different host than mentioned one then update axios-instance.js file ```baseURL``` value which is located
+inside ```frontend/src/axios-instance.js```
+
+* After completing all step your server will be up and running
+
+### Frontend Configuration
+
+* CD to frontend directory and run npm update
+* after dependency installation run command "npm start"
+* open browser and enter your *http://localhost:3000*
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ database/task.sql
@@ -0,0 +1,84 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 10.1.38-MariaDB : Database - task
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`task` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `task`;
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`id`,`migration`,`batch`) values (3,'2014_10_12_000000_create_users_table',1),(4,'2014_10_12_100000_create_password_resets_table',1),(5,'2019_10_15_152444_create_task_table',1);
+
+/*Table structure for table `password_resets` */
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `password_resets` */
+
+/*Table structure for table `tasks` */
+
+DROP TABLE IF EXISTS `tasks`;
+
+CREATE TABLE `tasks` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `tasks` */
+
+insert  into `tasks`(`id`,`title`,`description`) values (7,'PHP','adf asd f asdfsadf'),(8,'React JS','React JS React JS React JS');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
